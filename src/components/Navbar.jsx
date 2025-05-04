8d99e004976c98ddb9971d6e27d83e0ab4a1a0cb
Y1hy1 <163067395+y1hy11@users.noreply.github.com>
2025-05-04 07:31:00 +0100

    update footer and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,10 +45,10 @@ const Navbar = () => {
               <button className="nav-button language-btn">
                 <span>{t('nav.language')}</span>
                 <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                   <g id="SVGRepo_iconCarrier"> 
-                    <path d="M7 10L12 15L17 10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> 
+                    <path d="M7 10L12 15L17 10" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> 
                   </g>
                 </svg>
               </button>
